f1324ec2cbf22b5e7d5b9c56383a451118f0d27d
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-25 01:40:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var animal = 'cat'
+var animal = 'dog'
 
 function myAnimal() {
+  var animal = 'cat'
   return animal
-}
+  }
 
 function yourAnimal() {
   // How can we make sure that this function
